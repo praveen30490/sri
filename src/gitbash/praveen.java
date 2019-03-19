69e4221616f7fae682b2e68a1dd48be3ad51d25a
vetrikrrish <vetrikrrish@gmail.com>
2019-03-19 10:58:49 +0530

    file added

---
+++ src/gitbash/praveen.java
@@ -0,0 +1,15 @@
+package gitbash;
+
+public class praveen {
+
+	public static void main(String[] args) {
+		int[] num =new int[4];
+		num[0]=1;
+		num[1]=2;
+		num[2]=4;
+		num[3]=6;
+		System.out.println(num[3]);
+
+	}
+
+}
